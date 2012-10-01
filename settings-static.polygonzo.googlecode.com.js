4aa5d6bfde1c81c042166bc73c6f640ec1f0c7a1
Michael Geary <mg@mg.to>
2012-10-01 03:28:49 -0700

    Add settings file for Google Code

---
+++ settings-static.polygonzo.googlecode.com.js
@@ -0,0 +1,18 @@
+// settings-polygonzo.googlecode.com.js by Michael Geary
+// See LICENSE for copyright and license
+
+// To run this code in a different domain, copy this file
+// to a new file with a name like this:
+// settings-yourhostname.js
+// settings-yourhostname-yourportnumber.js
+// settings-www.example.com.js
+// settings-www.example.com-exampleportnumber.js
+//
+// Then edit the new file and change the key value below to your
+// Google API key for your host/domain name (and port number
+// if you have one):
+// http://code.google.com/apis/maps/signup.html
+
+var settings = {
+	apiKey: 'AIzaSyDgZgIBxOf1JSXAPSLJ0n3COBMrsBapGNI'
+};
